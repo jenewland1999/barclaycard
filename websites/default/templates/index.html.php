0b6371e625707c9e1ffa23df5c3f84f3e34e7b68
jenewland1999 <34895363+jenewland1999@users.noreply.github.com>
2019-02-20 12:16:46 +0000

    Updated hero carousel
    Signed-off-by: jenewland1999 <34895363+jenewland1999@users.noreply.github.com>

--- websites/default/templates/index.html.php
+++ websites/default/templates/index.html.php
@@ -1,74 +1,69 @@
-<!--Carousel Wrapper-->
-<div
-  id="video-carousel-example"
-  class="carousel slide carousel-fade"
-  data-ride="carousel"
->
-  <!--Indicators-->
-  <ol class="carousel-indicators">
-    <li
-      data-target="#video-carousel-example"
-      data-slide-to="0"
-      class="active"
-    ></li>
-    <li data-target="#video-carousel-example" data-slide-to="1"></li>
-    <li data-target="#video-carousel-example" data-slide-to="2"></li>
-  </ol>
-  <!--/.Indicators-->
-  <!--Slides-->
-  <div class="carousel-inner" role="listbox">
-    <div class="carousel-item active">
-      <video class="video-fluid" autoplay loop muted>
-        <source
-          src="https://mdbootstrap.com/img/video/Tropical.mp4"
-          type="video/mp4"
-        />
-      </video>
-    </div>
-    <div class="carousel-item">
-      <video class="video-fluid" autoplay loop muted>
-        <source
-          src="https://mdbootstrap.com/img/video/forest.mp4"
-          type="video/mp4"
-        />
-      </video>
-    </div>
-    <div class="carousel-item">
-      <video class="video-fluid" autoplay loop muted>
-        <source
-          src="https://mdbootstrap.com/img/video/Agua-natural.mp4"
-          type="video/mp4"
-        />
-      </video>
+<div class="container-fluid mx-0 px-0" style="">
+  <div class="row  mx-0 px-0">
+    <div class="col-12  mx-0 px-0">
+      <section
+        class="cta"
+        style="display: flex; align-items: center; position:absolute;top:50%; transform: translateY(-50%); left:4rem; z-index:9999; box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12); background-color: #ffffff; padding: 2rem; border-radius: 0.3rem; max-width:40rem;"
+      >
+        <figure class="cta__logo m-0 mr-3" style="flex: 0 0 33.333%;">
+          <img src="/images/logo.png" alt="" />
+        </figure>
+        <div class="cta__content" style="flex: 0 0 66.666%;">
+          <h3 class="cta__heading">Painting By Numbers</h3>
+          <p class="cta__text">
+            Painting By Numbers is a high street art retailer selling
+            high-quality, picturesque prints for their customers in Northampton,
+            UK.
+          </p>
+          <a href="/products" class="btn btn-dark m-0">View Products</a>
+        </div>
+      </section>
+
+      <!--Carousel Wrapper-->
+      <div
+        id="video-carousel-example"
+        class="carousel slide carousel-fade"
+        data-ride="carousel"
+      >
+        <!--Slides-->
+        <div class="carousel-inner" style="height: 50vh;" role="listbox">
+          <div class="carousel-item active">
+            <video class="video-fluid" autoplay loop muted>
+              <source
+                src="https://mdbootstrap.com/img/video/Tropical.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+          <div class="carousel-item">
+            <video class="video-fluid" autoplay loop muted>
+              <source
+                src="https://mdbootstrap.com/img/video/forest.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+          <div class="carousel-item">
+            <video class="video-fluid" autoplay loop muted>
+              <source
+                src="https://mdbootstrap.com/img/video/Agua-natural.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+        </div>
+        <!--/.Slides-->
+      </div>
+      <!--Carousel Wrapper-->
     </div>
   </div>
-  <!--/.Slides-->
-  <!--Controls-->
-  <a
-    class="carousel-control-prev"
-    href="#video-carousel-example"
-    role="button"
-    data-slide="prev"
-  >
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a
-    class="carousel-control-next"
-    href="#video-carousel-example"
-    role="button"
-    data-slide="next"
-  >
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-<!--Carousel Wrapper-->
+
 <div class="container mt-5">
-  <div class="row justify-content-around">
-    <h3 class="font-weight-light mb-5">Featured Products</h1>
-  </div>
-  <div class="row mb-3 justify-content-around">
+  <section class="row justify-content-around">
+    <h3 class="font-weight-light mb-5">Featured Products</h3>
+  </section>
+  <section class="row mb-3 justify-content-around">
     <div class="col-md-4">
       <div class="card">
         <img
@@ -78,11 +73,13 @@
         />
         <div class="card-body">
           <h5 class="card-title">James Photobomb</h5>
+          <h6 class="card-subtitle mb-2 text-muted">By Alex</h6>
+
           <p class="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
-          <a href="#" class="btn btn-primary">View Product</a>
+          <a href="#" class="btn btn-dark">View Product</a>
         </div>
       </div>
     </div>
@@ -95,11 +92,13 @@
         />
         <div class="card-body">
           <h5 class="card-title">The Screams</h5>
+          <h6 class="card-subtitle mb-2 text-muted">By Alex</h6>
+
           <p class="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
-          <a href="#" class="btn btn-primary">View Product</a>
+          <a href="#" class="btn btn-dark">View Product</a>
         </div>
       </div>
     </div>
@@ -112,22 +111,24 @@
         />
         <div class="card-body">
           <h5 class="card-title">Ryan and Horse</h5>
+          <h6 class="card-subtitle mb-2 text-muted">By James</h6>
+
           <p class="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
-          <a href="#" class="btn btn-primary">View Product</a>
+          <a href="#" class="btn btn-dark">View Product</a>
         </div>
       </div>
     </div>
+  </section>
 
-    <div class="row py-5 mt-5 bottom-banner">
-      <div class="col-lg-6 justify-content-around">
-        <h3 class="text-center">JOIN US AND GET A GIFT > ></h3>
-      </div>
-      <div class="col-lg-6 justify-content-around">
-        <a class="btn btn-dark float-left" id="lnk-signup2" href="#">SIGN UP</a>
-      </div>
+  <section class="row py-5 mt-5 bottom-banner align-items-center">
+    <div class="col-md-6 justify-content-around">
+      <h3 class="text-center mb-0">Join today, and receive 10% off.</h3>
     </div>
-  </div>
+    <div class="col-md-6 justify-content-around text-center">
+      <a class="btn btn-dark" id="lnk-signup2" href="#">SIGN UP</a>
+    </div>
+  </section>
 </div>
